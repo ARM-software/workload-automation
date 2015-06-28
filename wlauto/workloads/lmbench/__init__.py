cfb85b8ce7470ead190ec292b5f9939f5377aa3f
Bobby Batacharia <bobby.batacharia@arm.com>
2015-06-28 14:15:47 +0100

    Cleanups for pep8 / pylint

--- wlauto/workloads/lmbench/__init__.py
+++ wlauto/workloads/lmbench/__init__.py
@@ -22,24 +22,25 @@ from wlauto import Workload, Parameter, Executable
 from wlauto.exceptions import ConfigError, WorkloadError
 from wlauto.utils.types import list_or_integer, list_or_string
 
+
 class lmbench(Workload):
 
     name = 'lmbench'
 
     # Define supported tests. Each requires a _setup_{name} routine below
-    test_names = ['lat_mem_rd', 'bw_mem'] 
+    test_names = ['lat_mem_rd', 'bw_mem']
 
     description = """
-                  Run an lmbench subtest. 
+                  Run an lmbench subtest.
 
                   lmbench is a suite of simple, portable ANSI/C microbenchmarks for UNIX/POSIX. In general,
                   it measures two key features: latency and bandwidth. This workload supports a subset
                   of lmbench tests.
 
                   Original source from:
-                  http://sourceforge.net/projects/lmbench/. 
+                  http://sourceforge.net/projects/lmbench/.
                   See lmbench/bin/README for license details.
-                  """ 
+                  """
 
     parameters = [
         Parameter('test', default='lat_mem_rd', allowed_values=test_names,
@@ -47,23 +48,23 @@ class lmbench(Workload):
         Parameter('stride', kind=list_or_integer, default=[128],
                   description='Stride for lat_mem_rd test. Workload will iterate over one or more integer values.'),
         Parameter('thrash', kind=bool, default=True, description='Sets -t flag for lat_mem_rd_test'),
-        Parameter('size', kind=list_or_string, default="4m", 
+        Parameter('size', kind=list_or_string, default="4m",
                   description='Data set size for lat_mem_rd bw_mem tests.'),
-        Parameter('mem_category', kind=list_or_string, 
+        Parameter('mem_category', kind=list_or_string,
                   default=('rd', 'wr', 'cp', 'frd', 'fwr', 'fcp', 'bzero', 'bcopy'),
                   description='List of memory catetories for bw_mem test.'),
         Parameter('parallelism', kind=int, default=None, description='Parallelism flag for tests that accept it.'),
-        Parameter('warmup',      kind=int, default=None, description='Warmup flag for tests that accept it.'),        
-        Parameter('repetitions', kind=int, default=None, description='Repetitions flag for tests that accept it.'),        
-        Parameter('force_abi', kind=str, default=None, 
-                  description='Override device abi with this value. (Can be used to force arm32 on 64-bit devices.)'),
+        Parameter('warmup', kind=int, default=None, description='Warmup flag for tests that accept it.'),
+        Parameter('repetitions', kind=int, default=None, description='Repetitions flag for tests that accept it.'),
+        Parameter('force_abi', kind=str, default=None,
+                  description='Override device abi with this value. Can be used to force arm32 on 64-bit devices.'),
         Parameter('run_timeout', kind=int, default=900,
                   description="""
                   Timeout for execution of the test.
                   """),
         Parameter('times', kind=int, default=1, constraint=lambda x: x > 0,
                   description="""
-                  Specifies the number of times the benchmark will be run in a "tight loop", 
+                  Specifies the number of times the benchmark will be run in a "tight loop",
                   i.e. without performaing setup/teardown inbetween. This parameter is distinct from
                   "repetitions", as the latter takes place within the benchmark and produces a single result.
                   """),
@@ -73,7 +74,7 @@ class lmbench(Workload):
 
         abi = self.device.abi
         if self.force_abi:
-            abi=self.force_abi
+            abi = self.force_abi
 
         # self.test has been pre-validated, so this _should_ only fail if there's an abi mismatch
         host_exe = context.resolver.get(Executable(self, abi, self.test))
@@ -88,12 +89,12 @@ class lmbench(Workload):
 
         for time in xrange(self.times):
             for command in self.commands:
-                self.output.append("Output for time #{}, {}: ".format(time+1, command));
+                self.output.append("Output for time #{}, {}: ".format(time + 1, command))
                 self.output.append(self.device.execute(command, timeout=self.run_timeout, check_exit_code=False))
 
     def update_result(self, context):
         for output in self.output:
-            self.logger.debug(output);
+            self.logger.debug(output)
         outfile = os.path.join(context.output_directory, 'lmbench.output')
         with open(outfile, 'w') as wfh:
             for output in self.output:
@@ -103,13 +104,12 @@ class lmbench(Workload):
         self.device.uninstall_executable(self.test)
 
     def validate(self):
-        if not self.test in self.test_names:
-            raise WorkloadError("Unknown test '{}' specified. Choose from {}.".format(self.test,self.test_names))
+        if self.test not in self.test_names:
+            raise WorkloadError("Unknown test '{}' specified. Choose from {}.".format(self.test, self.test_names))
 
     #
     # Test setup routines
     #
-    
     def _setup_lat_mem_rd(self):
         command_stub = self._setup_common()
         if self.thrash:
@@ -119,14 +119,14 @@ class lmbench(Workload):
             command = command_stub + size + ' '
             for stride in self.stride:
                 self.commands.append(command + str(stride))
-    
+
     def _setup_bw_mem(self):
         command_stub = self._setup_common()
 
         for size in self.size:
             command = command_stub + size + ' '
             for what in self.mem_category:
-                self.commands.append(command + what)            
+                self.commands.append(command + what)
 
     def _setup_common(self):
         command = self.device_exe + " "
