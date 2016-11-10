46b78d35becf91306483606de5edba78cda20748
James Hartley <james.hartley@arm.com>
2016-11-10 18:56:22 +0000

    Geekbench 4 is similar in terms of operation but runs significantly different tests and hence produces a different results output.
    - Add UiAutomator and results parsing support for GB4
    - Remove the version checking in init.py and rely instead on the ApkWorkload version
      checking.  The required version in the agenda is checked against that on the host or target
    
    Tested with GB3.4.1 and GB4.0.0 on a Galaxy S7.  Older variants of the APK were not available.

--- wlauto/workloads/geekbench/uiauto/src/com/arm/wlauto/uiauto/UiAutomation.java
+++ wlauto/workloads/geekbench/uiauto/src/com/arm/wlauto/uiauto/UiAutomation.java
@@ -16,8 +16,6 @@
 
 package com.arm.wlauto.uiauto.geekbench;
 
-import java.util.concurrent.TimeUnit;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,52 +28,96 @@ import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
-import com.arm.wlauto.uiauto.BaseUiAutomation;
+import com.arm.wlauto.uiauto.UxPerfUiAutomation;
 
-public class UiAutomation extends BaseUiAutomation {   
+import java.util.concurrent.TimeUnit;
+
+public class UiAutomation extends UxPerfUiAutomation {
 
     public static String TAG = "geekbench";
+    public static final int WAIT_TIMEOUT_1SEC = 1000;
+    public static final long WAIT_TIMEOUT_5MIN = TimeUnit.SECONDS.toMillis(5 * 60);
+    public static final long WAIT_TIMEOUT_10MIN = TimeUnit.SECONDS.toMillis(10 * 60);
 
     public void runUiAutomation() throws Exception {
         Bundle params = getParams();
-        int version = Integer.parseInt(params.getString("version"));
+        String[] version = params.getString("version").split("\\.");
+        int majorVersion = Integer.parseInt(version[0]);
+        int minorVersion = Integer.parseInt(version[1]);
         int times = Integer.parseInt(params.getString("times"));
 
+        dismissEula();
+
         for (int i = 0; i < times; i++) {
-                runBenchmarks();
-                switch(version) {
-                case 2: 
-                        // In version 2, we scroll through the results WebView to make sure
-                        // all results appear on the screen, which causes them to be dumped into
-                        // logcat by the Linaro hacks.
-                        waitForResultsv2();
-                        scrollThroughResults();
-                        break;
-                case 3: 
+            switch (majorVersion) {
+                case 2:
+                    // In version 2, we scroll through the results WebView to make sure
+                    // all results appear on the screen, which causes them to be dumped into
+                    // logcat by the Linaro hacks.
+                    runBenchmarks();
+                    waitForResultsv2();
+                    scrollThroughResults();
+                    break;
+                case 3:
+                    runBenchmarks();
+                    waitForResultsv3onwards();
+                    if (minorVersion < 4) {
                         // Attempting to share the results will generate the .gb3 file with
                         // results that can then be pulled from the device. This is not possible
                         // in verison 2 of Geekbench (Share option was added later).
-                        waitForResultsv3();
+                        // Sharing is not necessary from 3.4.1 onwards as the .gb3 files are always
+                        // created.
                         shareResults();
-                        break;
-                }
+                    }
+                    break;
+                case 4:
+                    runCpuBenchmarks();
+                    waitForResultsv3onwards();
+                    break;
+                default :
+                    throw new RuntimeException("Invalid version of Geekbench requested");
+            }
 
-                if (i < (times - 1)) {
-                        getUiDevice().pressBack();
-                        getUiDevice().pressBack();  // twice
-                }
+            if (i < (times - 1)) {
+                getUiDevice().pressBack();
+                getUiDevice().pressBack();  // twice
+            }
         }
 
         Bundle status = new Bundle();
         getAutomationSupport().sendStatus(Activity.RESULT_OK, status);
     }
 
+    public void dismissEula() throws Exception {
+        UiObject acceptButton =
+            // new UiObject(new UiSelector().textContains("Accept")
+            new UiObject(new UiSelector().resourceId("android:id/button1")
+                                         .className("android.widget.Button"));
+        if (!acceptButton.waitForExists(WAIT_TIMEOUT_1SEC)) {
+            throw new UiObjectNotFoundException("Could not find Accept button");
+        }
+        acceptButton.click();
+    }
+
     public void runBenchmarks() throws Exception {
-        UiSelector selector = new UiSelector();
-        UiObject runButton = new UiObject(selector.text("Run Benchmarks")
-                                                  .className("android.widget.Button"));
-        if (!runButton.exists()) {
-            getUiDevice().pressBack();
+        UiObject runButton =
+            new UiObject(new UiSelector().textContains("Run Benchmarks")
+                                         .className("android.widget.Button"));
+        if (!runButton.waitForExists(WAIT_TIMEOUT_1SEC)) {
+            throw new UiObjectNotFoundException("Could not find Run button");
+        }
+        runButton.click();
+    }
+
+    public void runCpuBenchmarks() throws Exception {
+        // The run button is at the bottom of the view and may be off the screen so swipe to be sure
+        uiDeviceSwipe(Direction.DOWN, 50);
+
+        UiObject runButton =
+            new UiObject(new UiSelector().resourceId("com.primatelabs.geekbench:id/runCpuBenchmarks")
+                                         .className("android.widget.Button"));
+        if (!runButton.waitForExists(WAIT_TIMEOUT_1SEC)) {
+            throw new UiObjectNotFoundException("Could not find Run button");
         }
         runButton.click();
     }
@@ -83,18 +125,21 @@ public class UiAutomation extends BaseUiAutomation {
     public void waitForResultsv2() throws Exception {
         UiSelector selector = new UiSelector();
         UiObject resultsWebview = new UiObject(selector.className("android.webkit.WebView"));
-        if (!resultsWebview.waitForExists(TimeUnit.SECONDS.toMillis(200))) {
-                throw new UiObjectNotFoundException("Did not see Geekbench results screen.");
+        if (!resultsWebview.waitForExists(WAIT_TIMEOUT_5MIN)) {
+            throw new UiObjectNotFoundException("Did not see Geekbench results screen.");
         }
     }
 
-    public void waitForResultsv3() throws Exception {
+    public void waitForResultsv3onwards() throws Exception {
         UiSelector selector = new UiSelector();
         UiObject runningTextView = new UiObject(selector.text("Running Benchmarks...")
                                                         .className("android.widget.TextView"));
-        runningTextView.waitForExists(TimeUnit.SECONDS.toMillis(2));
-        if (!runningTextView.waitUntilGone(TimeUnit.SECONDS.toMillis(200))) {
-                throw new UiObjectNotFoundException("Did not get to Geekbench results screen.");
+
+        if (!runningTextView.waitForExists(WAIT_TIMEOUT_1SEC)) {
+            throw new UiObjectNotFoundException("Did not get to Running Benchmarks... screen.");
+        }
+        if (!runningTextView.waitUntilGone(WAIT_TIMEOUT_10MIN)) {
+            throw new UiObjectNotFoundException("Did not get to Geekbench results screen.");
         }
     }
 
@@ -110,12 +155,12 @@ public class UiAutomation extends BaseUiAutomation {
     }
 
     public void shareResults() throws Exception {
-	sleep(2); // transition
+        sleep(2); // transition
         UiSelector selector = new UiSelector();
         getUiDevice().pressMenu();
-        UiObject runButton = new UiObject(selector.text("Share")
-                                                  .className("android.widget.TextView"));
-        runButton.waitForExists(500);
-        runButton.click();
+        UiObject shareButton = new UiObject(selector.text("Share")
+                                                    .className("android.widget.TextView"));
+        shareButton.waitForExists(WAIT_TIMEOUT_1SEC);
+        shareButton.click();
     }
 }
