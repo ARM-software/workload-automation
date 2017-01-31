a54e3872a949ca3929b88b7fbb89016d74302f1a
jummp01 <jumana.mp@arm.com>
2017-01-31 17:34:45 +0000

    Move package parameters to UxperfUiAutomation
    All the uxperf workloads get some common package parameters.
    These are moved to the parent class and a new method is introduced to fill
    these parameter values. All the uxperf workloads can call this method to resolve
    the package parameters.

--- wlauto/external/uiauto/src/com/arm/wlauto/uiauto/UxPerfUiAutomation.java
+++ wlauto/external/uiauto/src/com/arm/wlauto/uiauto/UxPerfUiAutomation.java
@@ -16,8 +16,19 @@
 package com.arm.wlauto.uiauto;
 
 import java.util.logging.Logger;
+import android.os.Bundle;
 
 public class UxPerfUiAutomation extends BaseUiAutomation {
+    
+    protected Bundle parameters;
+    protected String packageName;
+    protected String packageID;
+
+    //Get application package parameters and create package ID
+    public void getPackageParameters() {
+        packageName = parameters.getString("package");
+        packageID = packageName + ":id/";
+    }
 
     private Logger logger = Logger.getLogger(UxPerfUiAutomation.class.getName());
 
