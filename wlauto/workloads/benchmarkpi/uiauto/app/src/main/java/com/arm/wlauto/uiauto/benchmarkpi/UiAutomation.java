0c7a7e4dcafc9dd94cba2a787c175f8ba48e91b7
Marc Bonnici <marc.bonnici@arm.com>
2017-05-09 09:43:50 +0100

    BenchmarkPi: Updated to uiautomator2

--- wlauto/workloads/benchmarkpi/uiauto/app/src/main/java/com/arm/wlauto/uiauto/benchmarkpi/UiAutomation.java
+++ wlauto/workloads/benchmarkpi/uiauto/app/src/main/java/com/arm/wlauto/uiauto/benchmarkpi/UiAutomation.java
@@ -18,45 +18,49 @@ package com.arm.wlauto.uiauto.benchmarkpi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
 import android.util.Log;
-
-// Import the uiautomator libraries
-import com.android.uiautomator.core.UiObject;
-import com.android.uiautomator.core.UiObjectNotFoundException;
-import com.android.uiautomator.core.UiScrollable;
-import com.android.uiautomator.core.UiSelector;
-import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 import com.arm.wlauto.uiauto.BaseUiAutomation;
 
-public class UiAutomation extends BaseUiAutomation {   
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+// Import the uiautomator libraries
+
+@RunWith(AndroidJUnit4.class)
+public class UiAutomation extends BaseUiAutomation {
 
     public static String TAG = "benchmarkpi";
 
-    public void runUiAutomation() throws Exception{
+@Test
+public void runUiAutomation() throws Exception{
+        initialize_instrumentation();
         Bundle status = new Bundle();
 
-	startTest();
-	waitForAndExtractResults();
+        startTest();
+        waitForAndExtractResults();
 
-        getAutomationSupport().sendStatus(Activity.RESULT_OK, status);
+        mInstrumentation.sendStatus(Activity.RESULT_OK, status);
     }
 
-    public void startTest() throws Exception{
+    public void startTest() throws Exception {
         UiSelector selector = new UiSelector();
-        UiObject benchButton = new UiObject(selector.text("Benchmark my Android!")
+        UiObject benchButton = mDevice.findObject(selector.text("Benchmark my Android!")
                                                     .className("android.widget.Button"));
         benchButton.click();
     }
 
     public void waitForAndExtractResults() throws Exception{
         UiSelector selector = new UiSelector();
-        UiObject submitButton = new UiObject(selector.text("Submit")
+        UiObject submitButton = mDevice.findObject(selector.text("Submit")
                                                      .className("android.widget.Button"));
-	submitButton.waitForExists(10 * 1000);
+        submitButton.waitForExists(10 * 1000);
 
-        UiObject resultsText = new UiObject(selector.textContains("You calculated Pi in")
-                                                    .className("android.widget.TextView"));
-	Log.v(TAG, resultsText.getText());
+        UiObject resultsText = mDevice.findObject(selector.textContains("You calculated Pi in")
+                                                        .className("android.widget.TextView"));
+        Log.v(TAG, resultsText.getText());
     }
 }
