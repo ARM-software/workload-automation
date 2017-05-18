c094a47f12c795278d4ca8d6533ba8ab7fe5f0a2
Marc Bonnici <marc.bonnici@arm.com>
2017-05-18 16:16:12 +0100

    Camerarecord: Updated to uiautomator2

--- wlauto/workloads/camerarecord/uiauto/app/src/main/java/com/arm/wlauto/uiauto/camerarecord/UiAutomation.java
+++ wlauto/workloads/camerarecord/uiauto/app/src/main/java/com/arm/wlauto/uiauto/camerarecord/UiAutomation.java
@@ -16,21 +16,19 @@
 
 package com.arm.wlauto.uiauto.camerarecord;
 
-import java.util.concurrent.TimeUnit;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-
-import com.android.uiautomator.core.UiObject;
-import com.android.uiautomator.core.UiObjectNotFoundException;
-import com.android.uiautomator.core.UiScrollable;
-import com.android.uiautomator.core.UiSelector;
-import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
 
 import com.arm.wlauto.uiauto.BaseUiAutomation;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(AndroidJUnit4.class)
 public class UiAutomation extends BaseUiAutomation {
 
     public static String TAG = "camerarecord";
@@ -42,7 +40,9 @@ public class UiAutomation extends BaseUiAutomation {
     int api = 0;
     Integer[] version = {0,0,0};
 
-    public void runUiAutomation() throws Exception {
+@Test
+public void runUiAutomation() throws Exception {
+        initialize_instrumentation();
         Bundle parameters = getParams();
         if (parameters.size() > 0) {
            recordingTime = parameters.getInt("recording_time");
@@ -54,78 +54,78 @@ public class UiAutomation extends BaseUiAutomation {
 
         //Pre Android M UI
         if (api < 23)
-            recordVideoAosp(); 
-        else 
+            recordVideoAosp();
+        else
         {
-            if(compareVersions(version, new Integer[]{3,2,0}) >= 0) 
+            if(compareVersions(version, new Integer[]{3,2,0}) >= 0)
                 recordVideoGoogleV3_2();
-            else 
+            else
                recordVideoGoogle();
-        } 
+        }
     }
 
     void recordVideoAosp() throws Exception {
         // switch to camera capture mode
-        UiObject clickModes = new UiObject(new UiSelector().descriptionMatches("Camera, video or panorama selector"));
+        UiObject clickModes = mDevice.findObject(new UiSelector().descriptionMatches("Camera, video or panorama selector"));
         clickModes.click();
         sleep(sleepTime);
 
-        UiObject changeModeToCapture = new UiObject(new UiSelector().descriptionMatches("Switch to video"));
+        UiObject changeModeToCapture = mDevice.findObject(new UiSelector().descriptionMatches("Switch to video"));
         changeModeToCapture.click();
         sleep(sleepTime);
 
-        UiObject clickRecordingButton = new UiObject(new UiSelector().descriptionMatches("Shutter button"));
+        UiObject clickRecordingButton = mDevice.findObject(new UiSelector().descriptionMatches("Shutter button"));
         clickRecordingButton.longClick();
         sleep(recordingTime);
 
         // Stop video recording
         clickRecordingButton.longClick();
-        getUiDevice().pressBack();
+        mDevice.pressBack();
     }
 
     void recordVideoGoogleV3_2() throws Exception {
         // clear tutorial if needed
-        UiObject tutorialText = new UiObject(new UiSelector().resourceId("com.android.camera2:id/photoVideoSwipeTutorialText"));
+        UiObject tutorialText = mDevice.findObject(new UiSelector().resourceId("com.android.camera2:id/photoVideoSwipeTutorialText"));
         if (tutorialText.waitForExists(TimeUnit.SECONDS.toMillis(5))) {
             tutorialText.swipeLeft(5);
             sleep(sleepTime);
             tutorialText.swipeRight(5);
         }
-        
+
         // ensure we are in video mode
-        UiObject viewFinder = new UiObject(new UiSelector().resourceId("com.android.camera2:id/viewfinder_frame"));
+        UiObject viewFinder = mDevice.findObject(new UiSelector().resourceId("com.android.camera2:id/viewfinder_frame"));
         viewFinder.swipeLeft(5);
 
         String captureButtonId = "com.android.camera2:id/photo_video_button";
         // Enable slow motion if requested
         if (recordingMode.equals("slow_motion")) {
-            getUiDevice().pressMenu();
-            UiObject slowMoButton = new UiObject(new UiSelector().text("Slow Motion").className("android.widget.TextView"));
+            mDevice.pressMenu();
+            UiObject slowMoButton = mDevice.findObject(new UiSelector().text("Slow Motion").className("android.widget.TextView"));
             slowMoButton.waitForExists(TimeUnit.SECONDS.toMillis(5));
             slowMoButton.click();
             captureButtonId = "com.android.camera2:id/video_hfr_shutter_button";
         }
 
         // click to capture photos
-        UiObject clickCaptureButton = new UiObject(new UiSelector().resourceId(captureButtonId));
+        UiObject clickCaptureButton = mDevice.findObject(new UiSelector().resourceId(captureButtonId));
         clickCaptureButton.longClick();
         sleep(recordingTime);
 
         // stop video recording
         clickCaptureButton.longClick();
     }
-    
-    void recordVideoGoogle() throws Exception { 
+
+    void recordVideoGoogle() throws Exception {
         // Open mode select menu
-        UiObject swipeScreen = new UiObject(new UiSelector().resourceId("com.android.camera2:id/mode_options_overlay"));
+        UiObject swipeScreen = mDevice.findObject(new UiSelector().resourceId("com.android.camera2:id/mode_options_overlay"));
         swipeScreen.swipeRight(5);
-        
+
         // Switch to video mode
-        UiObject changeModeToCapture = new UiObject(new UiSelector().descriptionMatches("Switch to Video Camera"));
+        UiObject changeModeToCapture = mDevice.findObject(new UiSelector().descriptionMatches("Switch to Video Camera"));
         changeModeToCapture.click();
         sleep(sleepTime);
 
-        UiObject clickRecordingButton = new UiObject(new UiSelector().descriptionMatches("Shutter"));
+        UiObject clickRecordingButton = mDevice.findObject(new UiSelector().descriptionMatches("Shutter"));
         clickRecordingButton.longClick();
         sleep(recordingTime);
 
