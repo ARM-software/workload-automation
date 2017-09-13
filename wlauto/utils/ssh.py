ARM-software/workload-automation

commit 51db53d979e877d80bfb57498f59bb657754ebb2

ssh: Back-port ssh_get_shell from devlib
This back-ports ssh_get_shell implementation from devlib. It includes
the following changes:

- original_prompt for Telnet verison of the connection can now be passed
  as an argument.
- Multiple attempts to connect with a timeout.
- Some additional implementation to the tty, including setting the size.

wlauto/utils/ssh.py
@@ -22,6 +22,7 @@ import re
 import threading
 import tempfile
 import shutil
+import time
 
 from pexpect import EOF, TIMEOUT, spawn, pxssh
 
@@ -36,21 +37,34 @@ sshpass = None
 logger = logging.getLogger('ssh')
 
 
-def ssh_get_shell(host, username, password=None, keyfile=None, port=None, timeout=10, telnet=False):
+def ssh_get_shell(host, username, password=None, keyfile=None, port=None, timeout=10, telnet=False, original_prompt=None):
     _check_env()
-    if telnet:
-        if keyfile:
-            raise ConfigError('keyfile may not be used with a telnet connection.')
-        conn = TelnetConnection()
-    else:  # ssh
-        conn = pxssh.pxssh()  # pylint: disable=redefined-variable-type
-    try:
-        if keyfile:
-            conn.login(host, username, ssh_key=keyfile, port=port, login_timeout=timeout)
-        else:
-            conn.login(host, username, password, port=port, login_timeout=timeout)
-    except EOF:
-        raise DeviceError('Could not connect to {}; is the host name correct?'.format(host))
+    start_time = time.time()
+    while True:
+        if telnet:
+            if keyfile:
+                raise ValueError('keyfile may not be used with a telnet connection.')
+            conn = TelnetPxssh(original_prompt=original_prompt)
+        else:  # ssh
+            conn = pxssh.pxssh()
+
+        try:
+            if keyfile:
+                conn.login(host, username, ssh_key=keyfile, port=port, login_timeout=timeout)
+            else:
+                conn.login(host, username, password, port=port, login_timeout=timeout)
+            break
+        except EOF:
+            timeout -= time.time() - start_time
+            if timeout <= 0:
+                message = 'Could not connect to {}; is the host name correct?'
+                raise TargetError(message.format(host))
+            time.sleep(5)
+
+    conn.setwinsize(500,200)
+    conn.sendline('')
+    conn.prompt()
+    conn.setecho(False)
     return conn
 
 

commit a3d042e05445beb585771f05b5f5fecb6ec9f735

utils/ssh: fix telnet connection
This fixes the ability to connect over telnet rather than SSH which was
broken by commit 51db53d9

	 ssh: Back-port ssh_get_shell from devlib

wlauto/utils/ssh.py
@@ -40,19 +40,24 @@ logger = logging.getLogger('ssh')
 def ssh_get_shell(host, username, password=None, keyfile=None, port=None, timeout=10, telnet=False, original_prompt=None):
     _check_env()
     start_time = time.time()
+    extra_login_args = {}
     while True:
         if telnet:
             if keyfile:
                 raise ValueError('keyfile may not be used with a telnet connection.')
-            conn = TelnetPxssh(original_prompt=original_prompt)
+            conn = TelnetConnection()
+            if original_prompt:
+                extra_login_args['original_prompt'] = original_prompt
+            if port is None:
+                port = 23
         else:  # ssh
             conn = pxssh.pxssh()
 
         try:
             if keyfile:
-                conn.login(host, username, ssh_key=keyfile, port=port, login_timeout=timeout)
+                conn.login(host, username, ssh_key=keyfile, port=port, login_timeout=timeout, **extra_login_args)
             else:
-                conn.login(host, username, password, port=port, login_timeout=timeout)
+                conn.login(host, username, password, port=port, login_timeout=timeout, **extra_login_args)
             break
         except EOF:
             timeout -= time.time() - start_time