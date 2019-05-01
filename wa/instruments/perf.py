94645972f466261eeb88326651ee51fec6f23787
Pierre-Clément Tosi <pierre-clement.tosi@arm.com>
2019-05-01 11:49:38 +0100

    instruments/perf: Support ALL perf subcommands
    Introduce an implementation of the PerfInstrument that is more generic
    than the previous one and which is expected to be able to handle all
    potential calls to perf (irrespective of the subcommand, flags, options
    or arguments being used) but which maintains backward compatibility with
    the previous implementation, targeting perf-stat.

--- wa/instruments/perf.py
+++ wa/instruments/perf.py
@@ -1,4 +1,4 @@
-#    Copyright 2013-2015 ARM Limited
+#    Copyright 2013-2019 ARM Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,74 +13,170 @@
 # limitations under the License.
 #
 
-
-# pylint: disable=unused-argument
+import collections
 import os
-import re
 
+from devlib.utils.cli import Command
 from devlib.trace.perf import PerfCollector
-
 from wa import Instrument, Parameter
 from wa.utils.types import list_or_string, list_of_strs
 
-PERF_COUNT_REGEX = re.compile(r'^(CPU\d+)?\s*(\d+)\s*(.*?)\s*(\[\s*\d+\.\d+%\s*\])?\s*$')
+__all__ = [
+    'PerfInstrument',
+]
+
+
+class YamlCommandDescriptor(collections.OrderedDict):
+
+    def __init__(self, yaml_dict):
+        super(YamlCommandDescriptor, self).__init__()
+        if isinstance(yaml_dict, YamlCommandDescriptor):
+            for k, v in yaml_dict.items():
+                self[k] = v
+            return
+        yaml_dict_copy = yaml_dict.copy()
+        for label, parameters in yaml_dict_copy.items():
+            self[label] = str(Command(kwflags_join=',',
+                                      kwflags_sep='=',
+                                      end_of_options='--',
+                                      **parameters))
+
+
+DEFAULT_EVENTS = ['migration', 'cs']
+DEFAULT_OPTIONSTRING = '-a'
 
 
 class PerfInstrument(Instrument):
 
     name = 'perf'
     description = """
-    Perf is a Linux profiling with performance counters.
+    Perf is a Linux profiling tool based on performance counters.
 
-    Performance counters are CPU hardware registers that count hardware events
-    such as instructions executed, cache-misses suffered, or branches
-    mispredicted. They form a basis for profiling applications to trace dynamic
-    control flow and identify hotspots.
+    Performance counters are typically CPU hardware registers (found in the
+    Performance Monitoring Unit) that count hardware events such as
+    instructions executed, cache-misses suffered, or branches mispredicted.
+    Because each ``event`` corresponds to a hardware counter, the maximum
+    number of events that can be tracked is imposed by the available hardware.
 
-    pref accepts options and events. If no option is given the default '-a' is
-    used. For events, the default events are migrations and cs. They both can
-    be specified in the config file.
+    By extension, performance counters, in the context of ``perf``, also refer
+    to so-called "software counters" representing events that can be tracked by
+    the OS kernel (e.g. context switches). As these are software events, the
+    counters are kept in RAM and the hardware virtually imposes no limit on the
+    number that can be used.
 
-    Events must be provided as a list that contains them and they will look like
-    this ::
+    This instrument allows a straight-forward way of calling ``perf stat``
+    through the named parameters ``optionstring`` and ``events``, which is the
+    default behaviour (see the defaults of these parameters).  However, it can
+    also be used through the more advanced ``commands`` dictionary which
+    provides a flexible access to all ways ``perf`` can be used.
 
-        perf_events = ['migrations', 'cs']
+    The ``pre_commands`` and ``post_commands`` are provided to suit those
+    ``perf`` commands that don't actually capture data (``list``, ``config``,
+    ``report``, ...).
 
-    Events can be obtained by typing the following in the command line on the
-    device ::
+    Commands are tagged with _labels_ which are used to define in which
+    directory they run. Therefore, a pair of commands (_e.g._ a `record`
+    followed by a `report`) sharing the same label can access the same files
+    while commands with different labels can use the same filename with the
+    guarantee of avoiding clashes.
 
-        perf list
+    Depending on the subcommand used, ``perf`` might require setting:
 
-    Whereas options, they can be provided as a single string as following ::
+        - ``/proc/sys/kernel/printk`` to ``4``
+        - ``/proc/sys/kernel/kptr_restrict`` to ``0``
 
-        perf_options = '-a -i'
-
-    Options can be obtained by running the following in the command line ::
-
-        man perf-stat
+    Please refer to the ``sysfile_values`` runtime parameter to do so from an
+    agenda.
     """
 
     parameters = [
-        Parameter('events', kind=list_of_strs, default=['migrations', 'cs'],
-                  global_alias='perf_events',
-                  constraint=(lambda x: x, 'must not be empty.'),
-                  description="""Specifies the events to be counted."""),
-        Parameter('optionstring', kind=list_or_string, default='-a',
-                  global_alias='perf_options',
-                  description="""Specifies options to be used for the perf command. This
-                  may be a list of option strings, in which case, multiple instances of perf
-                  will be kicked off -- one for each option string. This may be used to e.g.
-                  collected different events from different big.LITTLE clusters.
-                  """),
-        Parameter('labels', kind=list_of_strs, default=None,
-                  global_alias='perf_labels',
-                  description="""Provides labels for pref output. If specified, the number of
-                  labels must match the number of ``optionstring``\ s.
-                  """),
         Parameter('force_install', kind=bool, default=False,
                   description="""
-                  always install perf binary even if perf is already present on the device.
+                  Always install ``perf`` binary even if ``perf`` is already
+                  present on the device.
                   """),
+        Parameter('events', kind=list_of_strs, default=None,
+                  description="""
+                  List of events the default ``perf stat`` should capture.
+                  Valid events can be obtained from ``perf list`` and
+                  ``perf --help``.
+                  This parameter is ignored if ``commands`` is passed.
+
+                  default: {}
+                  """.format(
+                      ','.join('``{}``'.format(e) for e in DEFAULT_EVENTS))
+                  ),
+        Parameter('optionstring', kind=list_or_string, default=None,
+                  description="""
+                  String of options the default ``perf stat`` should use.
+                  For backward compatibility, this may be be a list of strings.
+                  In that case, a ``perf stat`` command will be launched for
+                  each string. This parameter is ignored if ``commands`` is
+                  passed.
+                  This parameter is ignored if ``commands`` is passed.
+
+                  default: ``{}``
+                  """.format(DEFAULT_OPTIONSTRING)
+                  ),
+        Parameter('labels', kind=list_of_strs, default=None,
+                  description=r"""
+                  These labels act like the keys of the ``commands`` parameter.
+                  They are provided for backward compatibility. If specified,
+                  the number of labels must match the number of
+                  ``optionstring``\ s. This parameter is ignored if
+                  ``commands`` is passed.
+                  """),
+        Parameter('pre_commands', kind=YamlCommandDescriptor, default=None,
+                  description="""
+                  Dictionary of commands to be run before the workloads run
+                  (same format as ``commands``).
+                 """),
+        Parameter('commands', kind=YamlCommandDescriptor, default=None,
+                  description="""
+                  Dictionary in which keys are considered as *labels* and
+                  values are themselves dictionaries with the following
+                  entries:
+
+                      - ``command`` (``str``): The ``perf`` subcommand
+                        (``stat``, ``record``, ...);
+                      - ``flags`` (``str`` or ``list``): Switch flags without
+                        their leading hyphens (``no-inherit``, ``all-cpus``,
+                        ``a``, ...);
+                      - ``kwflags`` (``dict``): Dictionary of flag names (no
+                        hyphen) as keys and their corresponding values.
+                        These values can be ``list``s for flags taking CSV
+                        inputs (``event``, ``pid``, ...);
+                      - ``args`` (``str`` or valid command): the post-``--``
+                        arguments. This is typically the command ``perf`` will
+                        launch and monitor. Therefore, a valid command
+                        dictionary (same as this one) is accepted;
+
+                  As an example, the default behaviour can be replicated
+                  through::
+
+                      :language: yaml
+
+                      perf:
+                          commands:
+                              default_behaviour:
+                                  command: stat
+                                  flags:
+                                      - all-cpus
+                                  kwflags:
+                                      event:
+                                          - migrations
+                                          - cs
+                                  args:
+                                      command: sleep
+                                      args: 1000
+                                  stderr: '&1'
+                                  stdout: stat.out
+                 """),
+        Parameter('post_commands', kind=YamlCommandDescriptor, default=None,
+                  description="""
+                  Dictionary of commands to be run after the workloads run
+                  (same format as ``commands``).
+                 """),
     ]
 
     def __init__(self, target, **kwargs):
@@ -88,51 +184,72 @@ class PerfInstrument(Instrument):
         self.collector = None
 
     def initialize(self, context):
+        # pylint: disable=unused-argument
+        # pylint: disable=access-member-before-definition
+        # pylint: disable=attribute-defined-outside-init
+        if self.commands is None:
+            if self.optionstring is None:
+                self.optionstring = DEFAULT_OPTIONSTRING
+
+            if self.events is None:
+                self.events = DEFAULT_EVENTS
+
+            if isinstance(self.optionstring, str):
+                self.optionstring = [self.optionstring]
+
+            if not self.labels:
+                self.labels = ['default{}'.format(i)
+                               for i, _ in enumerate(self.optionstring)]
+            elif isinstance(self.labels, str):
+                self.labels = [self.labels]
+
+            if len(self.labels) != len(self.optionstring):
+                raise ValueError('Lengths of labels and optionstring differ')
+
+            self.commands = YamlCommandDescriptor({
+                label: {
+                    'command': 'stat',
+                    'kwflags': {'event': self.events},
+                    'options': options,
+                    'args': {
+                        'command': 'sleep',
+                        'args': 1000,
+                    },
+                    'stderr': '&1',
+                    'stdout': 'stat.out',
+                }
+                for label, options in zip(self.labels, self.optionstring)
+            })
+        else:
+            for name in ['optionstring', 'events', 'labels']:
+                if self.__dict__[name] is not None:
+                    raise ValueError(
+                        '{} should not be passed if commands is'.format(name))
+
         self.collector = PerfCollector(self.target,
-                                       self.events,
-                                       self.optionstring,
-                                       self.labels,
-                                       self.force_install)
+                                       self.force_install,
+                                       self.pre_commands,
+                                       self.commands,
+                                       self.post_commands)
 
     def setup(self, context):
+        # pylint: disable=unused-argument
         self.collector.reset()
 
     def start(self, context):
+        # pylint: disable=unused-argument
         self.collector.start()
 
     def stop(self, context):
+        # pylint: disable=unused-argument
         self.collector.stop()
 
     def update_output(self, context):
-        self.logger.info('Extracting reports from target...')
         outdir = os.path.join(context.output_directory, 'perf')
-        self.collector.get_trace(outdir)
-
-        for host_file in os.listdir(outdir):
-            label = host_file.split('.out')[0]
-            host_file_path = os.path.join(outdir, host_file)
-            context.add_artifact(label, host_file_path, 'raw')
-            with open(host_file_path) as fh:
-                in_results_section = False
-                for line in fh:
-                    if 'Performance counter stats' in line:
-                        in_results_section = True
-                        next(fh)  # skip the following blank line
-                    if in_results_section:
-                        if not line.strip():  # blank line
-                            in_results_section = False
-                            break
-                        else:
-                            line = line.split('#')[0]  # comment
-                            match = PERF_COUNT_REGEX.search(line)
-                            if match:
-                                classifiers = {}
-                                cpu = match.group(1)
-                                if cpu is not None:
-                                    classifiers['cpu'] = int(cpu.replace('CPU', ''))
-                                count = int(match.group(2))
-                                metric = '{}_{}'.format(label, match.group(3))
-                                context.add_metric(metric, count, classifiers=classifiers)
+        self.collector.get_traces(outdir)
+        # HUGE TODO: add parsers for supported post_commands
+        #    (or should these be in devlib?)
 
     def teardown(self, context):
+        # pylint: disable=unused-argument
         self.collector.reset()
