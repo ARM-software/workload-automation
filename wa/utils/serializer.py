f930694fc869f395c1c92a7608be46c8553e252c
Marc Bonnici <marc.bonnici@arm.com>
2019-05-15 18:03:15 +0100

    utils/serializer: pylint fixes

--- wa/utils/serializer.py
+++ wa/utils/serializer.py
@@ -227,19 +227,19 @@ def _wa_cpu_mask_constructor(loader, node):
     return cpu_mask(value)
 
 
-class _WaYamlLoader(_yaml_loader):
+class _WaYamlLoader(_yaml_loader):  # pylint: disable=too-many-ancestors
 
     def construct_mapping(self, node, deep=False):
         if not isinstance(node, MappingNode):
             raise ConstructorError(None, None,
-                    "expected a mapping node, but found %s" % node.id,
-                    node.start_mark)
+                                   "expected a mapping node, but found %s" % node.id,
+                                   node.start_mark)
         mapping = OrderedDict()
         for key_node, value_node in node.value:
             key = self.construct_object(key_node, deep=deep)
             if not isinstance(key, Hashable):
                 raise ConstructorError("while constructing a mapping", node.start_mark,
-                        "found unhashable key", key_node.start_mark)
+                                       "found unhashable key", key_node.start_mark)
             value = self.construct_object(value_node, deep=deep)
             mapping[key] = value
         return mapping
