dc2fc99e98c834eac80ff345de53edf0318e8941
Marc Bonnici <marc.bonnici@arm.com>
2023-04-29 23:09:23 +0100

    fw/version: Bump release versions

--- wa/framework/version.py
+++ wa/framework/version.py
@@ -21,9 +21,9 @@ from subprocess import Popen, PIPE
 
 VersionTuple = namedtuple('Version', ['major', 'minor', 'revision', 'dev'])
 
-version = VersionTuple(3, 3, 1, 'dev1')
+version = VersionTuple(3, 3, 1, '')
 
-required_devlib_version = VersionTuple(1, 3, 1, 'dev2')
+required_devlib_version = VersionTuple(1, 3, 3, '')
 
 
 def format_version(v):
