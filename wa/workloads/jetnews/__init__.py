1e094280c7273c3ad197bbd6d6f3d12cd66a7896
Luis Machado <luis.machado@arm.com>
2023-10-31 14:36:31 +0000

    workloads: Update jetnews wa tests
    The jetnews wa workload runs a single test at the moment, but there are
    more tests available.
    
    This patch enables two additional basic tests and renames the wa workload
    name to "jetnews", in preparation to potential future updates where we will
    be able to select the kind of test we want to run.

--- wa/workloads/jetnews/__init__.py
+++ wa/workloads/jetnews/__init__.py
@@ -1,25 +1,32 @@
 from wa import Workload
 
-
-class JetNewsScrollArticle(Workload):
-    name = "jetnewsScrollArticle"
-    description = "Run the JetNews ScrollArticleTest"
+class JetNews(Workload):
+    name = "jetnews"
+    description = "Run the JetNews basic tests"
     package_names = ["com.example.jetnews", "com.arm.benchmark"]
 
     def setup(self, context):
-        super(JetNewsScrollArticle, self).setup(context)
+        super(JetNews, self).setup(context)
         for package in self.package_names:
             self.target.execute(f"am force-stop {package}")
 
     def run(self, context):
-        super(JetNewsScrollArticle, self).run(context)
-        self.target.execute((
-            "am instrument -w "
-            "-e class com.arm.test.ScrollArticleTest "
-            "com.arm.benchmark/androidx.test.runner.AndroidJUnitRunner"
-        ))
+        super(JetNews, self).run(context)
+
+        # Run the first set of basic tests:
+        tests = ["ScrollArticleTest",
+                 "ScrollArticleSlowlyTest",
+                 "UserSimulationTest"]
+
+        # Invoke each test one after the other.
+        for test in tests:
+            self.target.execute(
+                "am instrument -w "
+                "-e class com.arm.test." + test + " "
+                "com.arm.benchmark/androidx.test.runner.AndroidJUnitRunner"
+            )
 
     def teardown(self, context):
-        super(JetNewsScrollArticle, self).teardown(context)
+        super(JetNews, self).teardown(context)
         for package in self.package_names:
             self.target.execute(f"am force-stop {package}")
