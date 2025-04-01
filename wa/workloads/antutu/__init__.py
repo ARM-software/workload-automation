dcf017197866d08980fef208823873f49d602701
scojac01 <scott.jackson@arm.com>
2025-04-01 14:13:55 +0100

    Bumping Antutu versions

--- wa/workloads/antutu/__init__.py
+++ wa/workloads/antutu/__init__.py
@@ -14,7 +14,7 @@
 #
 import re
 
-from wa import ApkUiautoWorkload, WorkloadError, Parameter
+from wa import ApkUiautoWorkload, WorkloadError, Parameter, ApkFile
 
 
 class Antutu(ApkUiautoWorkload):
@@ -65,6 +65,26 @@ class Antutu(ApkUiautoWorkload):
                         re.compile(r'ROM Sequential Read Score (.+)'),
                         re.compile(r'ROM Sequential Write Score (.+)'),
                         re.compile(r'ROM Random Access Score (.+)')]
+    regex_matches_v10 = [re.compile(r'CPU Mathematical Operations Score (.+)'),
+                        re.compile(r'CPU Common Algorithms Score (.+)'),
+                        re.compile(r'CPU Multi-Core Score (.+)'),
+                        re.compile(r'GPU Seasons Score (.+)'),
+                        re.compile(r'GPU Coastline2 Score (.+)'),
+                        re.compile(r'RAM Bandwidth Score (.+)'),
+                        re.compile(r'RAM Latency Score (.+)'),
+                        re.compile(r'ROM APP IO Score (.+)'),
+                        re.compile(r'ROM Sequential Read Score (.+)'),
+                        re.compile(r'ROM Sequential Write Score (.+)'),
+                        re.compile(r'ROM Random Access Score (.+)'),
+                        re.compile(r'Data Security Score (.+)'),
+                        re.compile(r'Data Processing Score (.+)'),
+                        re.compile(r'Document Processing Score (.+)'),
+                        re.compile(r'Image Decoding Score (.+)'),
+                        re.compile(r'Image Processing Score (.+)'),
+                        re.compile(r'User Experience Score (.+)'),
+                        re.compile(r'Video CTS Score (.+)'),
+                        re.compile(r'Video Decoding Score (.+)'),
+                        re.compile(r'Video Editing Score (.+)')]
     description = '''
     Executes Antutu 3D, UX, CPU and Memory tests
 
@@ -75,7 +95,10 @@ class Antutu(ApkUiautoWorkload):
     Known working APK version: 8.0.4
     '''
 
-    supported_versions = ['7.0.4', '7.2.0', '8.0.4', '8.1.9', '9.1.6']
+    supported_versions = ['7.0.4', '7.2.0',
+            '8.0.4', '8.1.9', '8.4.5',
+            '9.1.6', '9.2.9',
+            '10.0.1-OB1', '10.0.6-OB6', '10.1.9', '10.2.1']
 
     parameters = [
         Parameter('version', kind=str, allowed_values=supported_versions, override=True,
@@ -90,11 +113,31 @@ class Antutu(ApkUiautoWorkload):
         super(Antutu, self).__init__(device, **kwargs)
         self.gui.timeout = 1200
 
+    def initialize(self, context):
+        super(Antutu, self).initialize(context)
+        #Install the supporting benchmark
+        supporting_apk = context.get_resource(ApkFile(self, package='com.antutu.benchmark.full'))
+        self.target.install(supporting_apk)
+        #Ensure the orientation is set to portrait
+        self.target.set_rotation(0)
+        #Change the logcat buffer to be the max size - required to avoid missing out on scores - alternatively use the logcat polling param - DOES NOT WORK
+        #cmd = "logcat -G 16M"
+        #self.target.execute(cmd)
+        #Launch adb logcat as a process
+        #print("Launching logcat")
+        #cmd = "logcat &> /data/local/tmp/logcat.log &"
+        #self.target.execute(cmd)
+        #print("Logcat launched")
+
     def setup(self, context):
         self.gui.uiauto_params['version'] = self.version
         super(Antutu, self).setup(context)
 
     def extract_scores(self, context, regex_version):
+        cpu = []
+        gpu = []
+        ux = []
+        mem = []
         #pylint: disable=no-self-use
         expected_results = len(regex_version)
         logcat_file = context.get_artifact_path('logcat')
@@ -105,17 +148,43 @@ class Antutu(ApkUiautoWorkload):
                     if match:
                         try:
                             result = float(match.group(1))
+                            #print("MATCHED")
+                            #print(regex)
                         except ValueError:
                             result = float('NaN')
                         entry = regex.pattern.rsplit(None, 1)[0]
                         context.add_metric(entry, result, lower_is_better=False)
+        #Calculate group scores if 'CPU' in entry:
+                        if 'CPU' in entry:
+                            cpu.append(result)
+                            cpu_result = sum(cpu)
+                        if 'GPU' in entry:
+                            gpu.append(result)
+                            gpu_result = sum(gpu)
+                        if any([i in entry for i in ['Data', 'Document', 'Image', 'User', 'Video']]):
+                            ux.append(result)
+                            ux_result = sum(ux)
+                        if any([i in entry for i in ['RAM', 'ROM']]):
+                            mem.append(result)
+                            mem_result = sum(mem)
                         expected_results -= 1
         if expected_results > 0:
             msg = "The Antutu workload has failed. Expected {} scores, Detected {} scores."
-            raise WorkloadError(msg.format(len(regex_version), expected_results))
+            #raise WorkloadError(msg.format(len(regex_version), expected_results))
+
+        context.add_metric('CPU Total Score', cpu_result, lower_is_better=False)
+        context.add_metric('GPU Total Score', gpu_result, lower_is_better=False)
+        context.add_metric('UX Total Score', ux_result, lower_is_better=False)
+        context.add_metric('MEM Total Score', mem_result, lower_is_better=False)
+
+        #Calculate overall scores
+        overall_result = float(cpu_result + gpu_result + ux_result + mem_result)
+        context.add_metric('Overall Score', overall_result, lower_is_better=False)
 
     def update_output(self, context):
         super(Antutu, self).update_output(context)
+        if self.version.startswith('10'):
+            self.extract_scores(context, self.regex_matches_v10)
         if self.version.startswith('9'):
             self.extract_scores(context, self.regex_matches_v9)
         if self.version.startswith('8'):
--- wa/workloads/antutu/uiauto/app/src/main/java/com/arm/wa/uiauto/antutu/UiAutomation.java
+++ wa/workloads/antutu/uiauto/app/src/main/java/com/arm/wa/uiauto/antutu/UiAutomation.java
@@ -65,7 +65,9 @@ public class UiAutomation extends BaseUiAutomation {
 
     @Test
     public void extractResults() throws Exception{
-        if (version.startsWith("9")){
+        if (version.startsWith("10")){
+            getScoresv10();
+        } else if (version.startsWith("9")){
             getScoresv9();
         } else if (version.startsWith("8")){
             getScoresv8();
@@ -82,6 +84,27 @@ public class UiAutomation extends BaseUiAutomation {
     }
 
     public void clearPopups() throws Exception {
+        //Refuse the first popup and accept the second as the first accept does not work
+        UiObject refuse = 
+            mDevice.findObject(new UiSelector().textContains("Refuse"));
+        if (refuse.exists()){
+            refuse.click();
+        }
+        UiObject accept = 
+            mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/start_privacy_policy_preview_ok"));
+        if (accept.exists()){
+            Log.d(TAG, "Accept exists");
+            accept.click();
+            Log.d(TAG, "Accept clicked");
+        }
+
+        //Some devices present a whitelist - allow the benchmark to use the network
+        UiObject whitelist =
+            mDevice.findObject(new UiSelector().text("ALLOW"));
+        if (whitelist.exists()){
+            whitelist.click();
+        }
+
         UiObject agreement = 
             mDevice.findObject(new UiSelector().textContains("NEXT"));
         agreement.waitForExists(5000);
@@ -97,10 +120,27 @@ public class UiAutomation extends BaseUiAutomation {
         }
     }
 
+    public void clearChainStartPopup() throws Exception {
+        UiObject accept =
+            mDevice.findObject(new UiSelector().textContains("Accept"));
+        accept.waitForExists(5000);
+        if (accept.exists()){
+            accept.click();
+        }
+    }
+
     public void waitforCompletion() throws Exception {
-        UiObject totalScore =
-            mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/textViewTotalScore"));
-        totalScore.waitForExists(600000);
+        clearChainStartPopup();
+        //Wait up to twenty minutes for the benchmark to complete
+        if (version.startsWith("10")){
+            UiObject totalScore =
+                mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/TextViewScoreValue"));
+            totalScore.waitForExists(12000000);
+        } else {
+            UiObject totalScore =
+                mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/textViewTotalScore"));
+            totalScore.waitForExists(12000000);
+	}
     }
 
     public void getScoresv7() throws Exception {
@@ -393,8 +433,232 @@ public class UiAutomation extends BaseUiAutomation {
             list.scrollIntoView(romaccess);
         }
         Log.d(TAG, "ROM Random Access Score " + romaccess.getText());
-        list.scrollToBeginning(10);        
+        list.scrollToBeginning(10);
         memscores.click();
     }
 
+    public void getScoresv10() throws Exception {
+        UiScrollable list = new UiScrollable(new UiSelector().scrollable(true));
+        UiObject endlist = mDevice.findObject(new UiSelector().text("Controllable Area Test"));
+        UiObject root = mDevice.findObject(new UiSelector().text("This device may be rooted"));
+        UiObject storage = mDevice.findObject(new UiSelector().text("Storage performance will degrade when available storage is less than 25%"));
+	    UiObject verification = mDevice.findObject(new UiSelector().text("This score has not been verified online"));
+
+        int offset = 0;
+        int indexvalue = 0;
+        if (verification.exists() || storage.exists()) {
+            offset += 1;
+        }
+        //Set the value for the index value which differs if the root warning is present
+        if (root.exists()) {
+            offset += 1;
+        }
+        Log.d(TAG, "offset: " + offset);
+
+        //Expand, Extract and Close GPU sub scores
+        indexvalue = 2;
+        UiObject gpuscores =
+            mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/RecyclerView"))
+            .getChild(new UiSelector().index(indexvalue + offset))
+            .getChild(new UiSelector().index(2));
+        gpuscores.click();
+        sleep(3);
+        UiObject gpuseasons =
+            mDevice.findObject(new UiSelector().text("Seasons - Vulkan").fromParent(new UiSelector().index(1)));
+        UiObject gpucoastline =
+            mDevice.findObject(new UiSelector().text("Coastline2 - Vulkan").fromParent(new UiSelector().index(1)));
+        Log.d(TAG, "GPU Seasons Score " + gpuseasons.getText());
+        Log.d(TAG, "GPU Coastline2 Score " + gpucoastline.getText());
+        gpuscores.click();
+
+        //Expand, Extract and Close CPU sub scores
+        indexvalue = 1;
+        UiObject cpuscores =
+            mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/RecyclerView"))
+            .getChild(new UiSelector().index(indexvalue + offset))
+            .getChild(new UiSelector().index(2));
+        cpuscores.click();
+        sleep(3);
+	    Log.d(TAG, "indexvalue:" + indexvalue);
+	    Log.d(TAG, "cpuscores clicked");
+        UiObject cpumaths =
+            mDevice.findObject(new UiSelector().text("CPU Mathematical Operations").fromParent(new UiSelector().index(1)));
+        UiObject cpucommon =
+            mDevice.findObject(new UiSelector().text("CPU Common Algorithms").fromParent(new UiSelector().index(1)));
+        UiObject cpumulti =
+            mDevice.findObject(new UiSelector().text("CPU Multi-Core").fromParent(new UiSelector().index(1)));
+        Log.d(TAG, "CPU Mathematical Operations Score " + cpumaths.getText());
+        Log.d(TAG, "CPU Common Algorithms Score " + cpucommon.getText());
+        Log.d(TAG, "CPU Multi-Core Score " + cpumulti.getText());
+        cpuscores.click();
+
+        //Expand, Extract and Close MEM sub scores
+        indexvalue = 3;
+        UiObject memscores =
+            mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/RecyclerView"))
+            .getChild(new UiSelector().index(indexvalue + offset))
+            .getChild(new UiSelector().index(2));
+        memscores.click();
+        UiObject rambw =
+            mDevice.findObject(new UiSelector().text("RAM BandWidth").fromParent(new UiSelector().index(1)));
+        UiObject ramlat =
+            mDevice.findObject(new UiSelector().text("RAM Latency").fromParent(new UiSelector().index(1)));
+        UiObject romapp =
+            mDevice.findObject(new UiSelector().text("ROM APP IO").fromParent(new UiSelector().index(1)));
+        UiObject romread =
+            mDevice.findObject(new UiSelector().text("ROM Sequential Read").fromParent(new UiSelector().index(1)));
+        UiObject romwrite =
+            mDevice.findObject(new UiSelector().text("ROM Sequential Write").fromParent(new UiSelector().index(1)));
+        UiObject romaccess =
+            mDevice.findObject(new UiSelector().text("ROM Random Access").fromParent(new UiSelector().index(1)));
+       if (!rambw.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding RAM Bandwidth");
+            list.scrollIntoView(rambw);
+        }
+        Log.d(TAG, "RAM Bandwidth Score " + rambw.getText());
+       if (!ramlat.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding RAM Latency");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(ramlat);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "RAM Latency Score " + ramlat.getText());
+        if (!romapp.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding ROM App");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(romapp);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "ROM APP IO Score " + romapp.getText());
+        if (!romread.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding ROM Read");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(romread);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "ROM Sequential Read Score " + romread.getText());
+        if (!romwrite.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding ROM Write");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(romwrite);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "ROM Sequential Write Score " + romwrite.getText());
+        if (!romaccess.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding ROM Access");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(romaccess);
+            if (endlist.exists()) {
+                list.scrollBackward(50);
+            }
+        }
+        Log.d(TAG, "ROM Random Access Score " + romaccess.getText());
+        list.flingToBeginning(10);
+        memscores.click();
+
+        //Expand, Extract and Close UX sub scores
+        indexvalue = 4;
+        UiObject uxscores =
+            mDevice.findObject(new UiSelector().resourceId("com.antutu.ABenchMark:id/RecyclerView"))
+            .getChild(new UiSelector().index(indexvalue + offset))
+            .getChild(new UiSelector().index(2));
+        uxscores.click();
+        UiObject security =
+            mDevice.findObject(new UiSelector().text("Data Security").fromParent(new UiSelector().index(1)));
+        UiObject dataprocessing =
+            mDevice.findObject(new UiSelector().text("Data Processing").fromParent(new UiSelector().index(1)));
+        UiObject docprocessing =
+            mDevice.findObject(new UiSelector().text("Document Processing").fromParent(new UiSelector().index(1)));
+        UiObject imagedecoding =
+            mDevice.findObject(new UiSelector().text("Image Decoding").fromParent(new UiSelector().index(1)));
+        UiObject imageprocessing =
+            mDevice.findObject(new UiSelector().text("Image Processing").fromParent(new UiSelector().index(1)));
+        UiObject uxscore =
+            mDevice.findObject(new UiSelector().text("User Experience").fromParent(new UiSelector().index(1)));
+        UiObject videocts =
+            mDevice.findObject(new UiSelector().text("Video CTS").fromParent(new UiSelector().index(1)));
+        UiObject videodecode =
+            mDevice.findObject(new UiSelector().text("Video Decoding").fromParent(new UiSelector().index(1)));
+        UiObject videoedit =
+            mDevice.findObject(new UiSelector().text("Video Editing").fromParent(new UiSelector().index(1)));
+        if (!security.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Data Security");
+            list.scrollIntoView(security);
+        }
+        Log.d(TAG, "Data Security Score " + security.getText());
+        if (!dataprocessing.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Data Security");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(dataprocessing);
+        }
+        Log.d(TAG, "Data Processing Score " + dataprocessing.getText());
+        if (!docprocessing.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Data Processing");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(docprocessing);
+        }
+        Log.d(TAG, "Document Processing Score " + docprocessing.getText());
+        if (!imagedecoding.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Image Decoding");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(imagedecoding);
+        }
+        Log.d(TAG, "Image Decoding Score " + imagedecoding.getText());
+
+        if (!imageprocessing.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Image Processing");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(imageprocessing);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "Image Processing Score " + imageprocessing.getText());
+        if (!uxscore.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding UXScore");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(uxscore);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "User Experience Score " + uxscore.getText());
+        if (!videocts.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Video CTS");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(videocts);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "Video CTS Score " + videocts.getText());
+        if (!videodecode.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Video Decode");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(videodecode);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "Video Decoding Score " + videodecode.getText());
+        if (!videoedit.exists() && list.waitForExists(60)) {
+            Log.d(TAG, "Finding Video Edit");
+            list.scrollToBeginning(5);
+            list.scrollIntoView(videoedit);
+            if (endlist.exists()) {
+                list.scrollBackward();
+            }
+        }
+        Log.d(TAG, "Video Editing Score " + videoedit.getText());
+        list.scrollToBeginning(10);
+        uxscores.click();
+    }
 }
